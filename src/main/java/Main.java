685a2618b1125fa5d94a9ffb250da85b5582eb06
labayvetroi <namnguyenuetk62@gmail.com>
2020-07-21 08:39:13 +0700

    complete count record

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,57 @@
 import io.minio.GetObjectArgs;
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.errors.*;
+import io.minio.messages.Item;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-@lombok.extern.slf4j.Slf4j
 public class Main {
     public static void main(String[] args) {
-        int record_counter = 0;
         MinioClient minioClient = MinioClient.builder()
                 .endpoint("http://localhost:9000")
                 .credentials("minioadmin", "minioadmin")
                 .build();
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder().bucket("test").build());
+        for (Result<Item> result : results) {
+            try {
+                String objectName = result.get().objectName();
+                System.out.println(result.get().objectName());
+                int nuOfRecords = countRecord(minioClient,objectName);
+                System.out.println(nuOfRecords);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+//            try {
+//                System.out.println(result.get());
+//            }
+//            catch (Exception e ) {
+//                System.out.println(e.toString()
+//                );
+//            }
+        }
+    }
+
+    public static int countRecord(MinioClient minioClient, String objectName){
+        int record_counter = 0;
         try (InputStream stream = minioClient.getObject(GetObjectArgs.builder()
-                .bucket("my-bucketname")
-                .object("my-objectname")
+                .bucket("test")
+                .object(objectName)
                 .build());
              BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         ) {
-            while(reader.ready()) {
-                reader.readLine();
+            while(reader.readLine() != null) {
                 record_counter += 1;
             }
         } catch (Exception e) {
             System.out.println(e.toString());
         }
-        System.out.println(record_counter);
+        return record_counter;
     }
 }
